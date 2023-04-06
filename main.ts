5d7bf273939f4760fc0ea6362044dd10b8a1b86a
Sevnet <sevnet@mail.ru>
2023-04-06 22:00:15 +0300

    well done

--- main.ts
+++ main.ts
@@ -1,2 +1,16 @@
 import 'uno.css';
 import '@unocss/reset/tailwind.css';
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask = document.getElementById("popupCreateTask");
+const domCloseBtn = document.getElementById("closeBtn");
+
+domBtnCreateTask.onclick = () => {
+  console.log("click");
+  domPopupCreateTask.classList.toggle("hidden");
+  domCloseBtn.onclick = () => {
+    console.log("click");
+    domPopupCreateTask.classList.toggle("hidden");
+    domCloseBtn.onclick = null;
+  }
+}
