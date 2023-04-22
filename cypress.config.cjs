299ccee7ad32bf6fe027e9b884f49d7097133b31
Sevnet <sevnet@mail.ru>
2023-04-22 10:42:21 +0300

    dsfdsf

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,7 +1,8 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
+	video: false,
 	e2e: {
-		baseUrl: 'http://localhost:5173/',
+		baseUrl: 'http://localhost:4173/',
 	},
 })
--- cypress/e2e/todos.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Create Todo', () => {
-    it('user open main page and create task', () => {
-      cy.visit('http://localhost:5173/');
-    })
-  }
-);
